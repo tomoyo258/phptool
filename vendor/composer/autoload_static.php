6b6b1620e4c2f4018fb02d7f8718d46bd8755cff
Tomoyo <2547943495@qq.com>
2023-10-08 16:40:33 +0800

    初始版本库

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,20 +6,6 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit6fe9653b770f57ec8064c17f4f8b2e3c
 {
-    public static $prefixLengthsPsr4 = array (
-        's' => 
-        array (
-            'src\\' => 4,
-        ),
-    );
-
-    public static $prefixDirsPsr4 = array (
-        'src\\' => 
-        array (
-            0 => __DIR__ . '/../..' . '/src',
-        ),
-    );
-
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -27,8 +13,6 @@ class ComposerStaticInit6fe9653b770f57ec8064c17f4f8b2e3c
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit6fe9653b770f57ec8064c17f4f8b2e3c::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit6fe9653b770f57ec8064c17f4f8b2e3c::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit6fe9653b770f57ec8064c17f4f8b2e3c::$classMap;
 
         }, null, ClassLoader::class);
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'name' => 'tomoyo/phptool',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8f100b762f16409eb0e579955d99484df007f389',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+        'tomoyo/phptool' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8f100b762f16409eb0e579955d99484df007f389',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
